06f6f3c9792513e13762e426243822ef7c5c6718
t_turecky <t_turecky@utb.cz>
2021-11-22 11:08:03 +0100

    Commit of the finished exercise

---
+++ app/src/main/java/cz/utb/fai/apiapp/MainActivity.java
@@ -0,0 +1,75 @@
+package cz.utb.fai.apiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.translated_text);
+        editText = (EditText) findViewById(R.id.text_for_translation);
+    }
+
+    public void getTranslationOnClick(View v) {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://api.mymemory.translated.net/get?q=" + editText.getText().toString() + "&langpair=en|cs";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // ZPRACOVANI JSONu:
+                        try {
+                            //1. Z DAT, KTERA JSME OBDRZELI VYTVORIME JSONObject
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            // 2. Z PROMENNE jsonObject ZISKAME "responseData" (viz struktura JSONu odpovedi)
+                            JSONObject responseData = jsonObject.getJSONObject("responseData");
+
+                            // 3. Z PROMENNE responseData ZISKAME "translatedText" (viz struktura JSONu odpovedi)
+                            // V PROMENNE translatedText JE ULOZEN VYSLEDEK PREKLADU
+                            String translatedText = responseData.getString("translatedText");
+
+                            // 4. V textView ZOBRAZIME VYSLEDEK PREKLADU
+                            textView.setText(editText.getText().toString() + " --> " + translatedText);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText("That didn't work!");
+                    }
+                });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+}
